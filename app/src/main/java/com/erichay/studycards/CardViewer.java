31e5eed3188f6113bce867967e6e4981fa045443
Eric Hay <erichay@live.ca>
2015-10-19 11:49:47 -0700

    Initial Commit

--- app/src/main/java/com/erichay/studycards/CardViewer.java
+++ app/src/main/java/com/erichay/studycards/CardViewer.java
@@ -60,6 +60,7 @@ public class CardViewer extends Activity
             return;
         }
 
+
         //Some variables we'll need
         String line; //This will hold an entire line - an entire card
         String definition; //This will hold the definition part of the card
